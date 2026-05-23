b905a243162c332ab20d716c3a7933ef993cb0af
naninetha05 <saikiran.gunda07@gmail.com>
2026-05-23 10:55:27 +0530

    Fix S3 backend bucket name and replace deprecated dynamodb_table with use_lockfile

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "your-terraform-state-bucket"
-    key            = "ci-cd-project/terraform.tfstate"
-    region         = "ap-south-1"
-    encrypt        = true
-    dynamodb_table = "terraform-state-lock"
+    bucket       = "ci-cd-pipline-projeact"
+    key          = "ci-cd-project/terraform.tfstate"
+    region       = "ap-south-1"
+    encrypt      = true
+    use_lockfile = true
   }
 }
--- terraform/terraform.tfvars
+++ terraform/terraform.tfvars
@@ -1,3 +1,3 @@
 aws_region     = "ap-south-1"
 key_pair_name  = "project-key"
-s3_bucket_name = "your-terraform-state-bucket"
+s3_bucket_name = "ci-cd-pipline-projeact"
