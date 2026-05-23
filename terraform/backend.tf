a30a6ac67e6d82bd3b8daeae409d199c811c7293
naninetha05 <saikiran.gunda07@gmail.com>
2026-05-23 11:52:53 +0530

    Remove use_lockfile from backend - not supported in Terraform 1.5

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,9 +1,8 @@
 terraform {
   backend "s3" {
-    bucket       = "ci-cd-pipline-projeact"
-    key          = "ci-cd-project/terraform.tfstate"
-    region       = "ap-south-1"
-    encrypt      = true
-    use_lockfile = true
+    bucket  = "ci-cd-pipline-projeact"
+    key     = "ci-cd-project/terraform.tfstate"
+    region  = "ap-south-1"
+    encrypt = true
   }
 }
